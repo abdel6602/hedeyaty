ab8592ef81d42ab1459673de8b5c79dc6ae723f4
Abdelrahman <abdelrahmangamal66002@gmail.com>
2024-12-10 21:12:55 +0200

    login page ui basic

---
+++ lib/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryDark = Color(0xFF201E1F);
+  static const primaryLight = Color(0xFFE0E0E0);
+  static const secondaryDark = Color(0xFF28965A);
+  static const secondaryLight = Color(0xFF0874cc);
+  static const thirdDark = Color(0xFFE0B941);
+  static const thirdLight = Color.fromARGB(255, 201, 153, 11);
+  static const textColor = Color(0xFFE8E8E8);
+}
